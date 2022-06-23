71c41d214303312f3c775b243ee2b8eb25c95e58
homePoliceMemos <policememos@gmail.com>
2022-06-23 21:15:18 +0500

    local network works

--- LocalNet.py
+++ LocalNet.py
@@ -9,23 +9,25 @@ class Server:
         self.ip = self.ip_creator()
         self.to_send = list()
         self.buffer = list()
+
+    @staticmethod    
+    def send_data(data):
+        router.get_data(data)
         
-    def send_data(self, data):
-        ...
-        
-    def get_data(self, data):
-        ...
+    def save_data(self, data):
+        self.buffer.append(data)
         
     def get_ip(self):
         return self.ip
+    
+    def get_data(self):
+        return self.buffer
         
         
 class Router:
     __instance: None|object = None
     
     __buffer = []
-    
-    servers_list = []
     
     __adress_table = dict()
     
@@ -35,19 +37,32 @@ class Router:
             return cls.__instance
         return cls.__instance
     
-    def __init__(self):
-        for serv in self.servers_list:
-            self.__adress_table.fromkeys
+    # def __init__(self):
+    #     for serv in self.servers_list:
+    #         self.__adress_table.fromkeys
     
     def link(self, server):
-        ...
+        self.__adress_table.setdefault(server.ip, server)
         
     def unlink(self, server):
         ...
+
+    @classmethod    
+    def get_data(cls, packet):
+        cls.__buffer.append(packet)
     
-    @staticmethod
-    def send_data(data):
-        ...
+    @classmethod
+    def clear_buffer(cls):
+        cls.__buffer.clear()
+        
+    @classmethod
+    def send_data(cls):
+        for pack in cls.__buffer:
+            ip_dest = pack.ip_dest
+            for ip, server in cls.__adress_table.items():
+                if ip == ip_dest:
+                    server.save_data(pack)
+        cls.clear_buffer()
         
     
 class Data:
@@ -56,16 +71,19 @@ class Data:
         self.ip_dest = ip
     
     
-    
-sv = Server()
-print(f'{sv.ip=}')
-sv2 = Server()
-print(f'{sv2.ip=}')
-sv3 = Server()
-print(f'{sv3.ip=}')
+router = Router()
+sv_from = Server()
+router.link(sv_from)
+router.link(Server())
+router.link(Server())
+sv_to = Server()
+router.link(sv_to)
+sv_from.send_data(Data("Hello", sv_to.get_ip()))
+router.send_data()
+sv_to.send_data(Data("Hi", sv_from.get_ip()))
+router.send_data()
+msg_lst_from = sv_from.get_data()
+msg_lst_to = sv_to.get_data()
 
-rt = Router()
-rt.servers_list = [sv,sv2,sv3]
-
-data = Data(строка с данными, IP-адрес назначения)
-
+print(msg_lst_from[0].data)
+print(msg_lst_to[0].data)
